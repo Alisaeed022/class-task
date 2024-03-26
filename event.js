d1f41fc81e2d30d9a346c5c266a97b35c4d9b128
Alisaeed022 <165067576+Alisaeed022@users.noreply.github.com>
2024-03-26 22:45:10 +0500

    Add files via upload

---
+++ event.js
@@ -0,0 +1,15 @@
+function update(){
+console. log("event is fired");}
+// I want to create variable
+var x =10;
+var y = 205
+console. log(x*y) ;
+//i want to create function is js
+function sum(a, b){
+    return a+b;
+}
+    
+        console. log/sum(x,y) ;
+        // If i want to assign function to variable
+        var mysum = sum;
+        console. log (mysum);
